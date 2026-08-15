79db17c5815ac15b13c7b4dc1007ed09d9004231
Meow Dev 7 <meowdev7official@gmail.com>
2026-08-15 08:36:35 +0530

    feat: basic json handling

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Student struct {
+	StudentInfo struct {
+		Name    string `json:"name"`
+		Class   string `json:"class"`
+		Section string `json:"section"`
+		RollNo  string `json:"roll_no"`
+	} `json:"student"`
+
+	Answers []string `json:"answers"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	data, err := os.ReadFile("../test/sample.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	var stu Student
+	err = json.Unmarshal(data, &stu)
+	if err != nil {
+		panic(err)
+	}
+	formattedJson, err := json.MarshalIndent(stu, "", " ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", formattedJson)
 }
